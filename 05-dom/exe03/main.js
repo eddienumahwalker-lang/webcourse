fb2688ab82dd98e0233db44f03cfc73535c02dfe
Eddie Numah Walker <eddienumahwalker@gmail.com>
2026-07-08 13:06:17 +0200

    Esircizio 04 - Manipolazione del DOM

--- 05-dom/exe03/main.js
+++ 05-dom/exe03/main.js
@@ -1,1 +1,12 @@
-  document.body.classList.toggle('theme');
+
+    const toggleButton = document.querySelector('.btn');
+
+      toggleButton.addEventListener('click', () => {
+        
+        document.body.classList.toggle('theme');
+        
+        if (document.body.classList.contains('theme')) {
+          toggleButton.textContent = 'Switch to Light Mode';
+        } else {
+          toggleButton.textContent = 'Switch to Dark Mode';
+        }
